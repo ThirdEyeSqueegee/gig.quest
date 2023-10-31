2e95068ccb9e7ec7b962edaee9212a321a0574e0
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2023-10-31 15:23:14 -0700

    Add other event types, add more range options

--- src/ShowsTable.tsx
+++ src/ShowsTable.tsx
@@ -15,9 +15,22 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Events, IEvent, Performer } from "./Interfaces";
 import {
+  DirectionsCar,
+  Diversity3,
   KeyboardArrowLeft,
   KeyboardArrowRight,
   KeyboardDoubleArrowLeft,
+  LocalActivity,
+  Mood,
+  MusicNote,
+  PlayCircle,
+  SportsBasketball,
+  SportsEsports,
+  SportsFootball,
+  SportsHockey,
+  SportsSoccer,
+  SportsVolleyball,
+  TheaterComedy,
 } from "@mui/icons-material";
 
 export default function ShowsTable() {
@@ -32,21 +45,132 @@ export default function ShowsTable() {
         params: {
           geoip: true,
           range: range,
-          per_page: rowsPerPage * 2,
+          per_page: rowsPerPage,
           page: page,
           client_id: import.meta.env.VITE_SEATGEEK_CLIENT_ID,
           client_secret: import.meta.env.VITE_SEATGEEK_CLIENT_SECRET,
         },
       })
       .then((response) => {
-        setEvents(
-          response.data
-            .events!.filter(
-              (event) => event.type === "concert" || event.type === "classical"
-            )
-            .slice(0, rowsPerPage)
-        );
+        setEvents(response.data.events!);
       });
+  };
+
+  const setEventTypeIcon = (eventType: string) => {
+    switch (eventType) {
+      case "nba":
+        return (
+          <Tooltip title="NBA" variant="soft" size="lg">
+            <SportsBasketball />
+          </Tooltip>
+        );
+      case "ncaa_basketball":
+      case "ncaa_womens_basketball":
+        return (
+          <Tooltip title="NCAA Basketball" variant="soft" size="lg">
+            <SportsBasketball />
+          </Tooltip>
+        );
+      case "ncaa_soccer":
+        return (
+          <Tooltip title="NCAA Soccer" variant="soft" size="lg">
+            <SportsSoccer />
+          </Tooltip>
+        );
+      case "mls":
+        return (
+          <Tooltip title="MLS" variant="soft" size="lg">
+            <SportsSoccer />
+          </Tooltip>
+        );
+      case "ncaa_football":
+        return (
+          <Tooltip title="NCAA Football" variant="soft" size="lg">
+            <SportsFootball />
+          </Tooltip>
+        );
+      case "concert":
+        return (
+          <Tooltip title="Concert" variant="soft" size="lg">
+            <PlayCircle />
+          </Tooltip>
+        );
+      case "music_festival":
+        return (
+          <Tooltip title="Music Festival" variant="soft" size="lg">
+            <PlayCircle />
+          </Tooltip>
+        );
+      case "classical":
+        return (
+          <Tooltip title="Classical" variant="soft" size="lg">
+            <MusicNote />
+          </Tooltip>
+        );
+      case "classical_opera":
+        return (
+          <Tooltip title="Opera" variant="soft" size="lg">
+            <MusicNote />
+          </Tooltip>
+        );
+      case "classical_orchestral_instrumental":
+        return (
+          <Tooltip title="Orchestra" variant="soft" size="lg">
+            <MusicNote />
+          </Tooltip>
+        );
+      case "broadway_tickets_national":
+      case "theater":
+        return (
+          <Tooltip title="Theater" variant="soft" size="lg">
+            <TheaterComedy />
+          </Tooltip>
+        );
+      case "esports":
+        return (
+          <Tooltip title="Esports" variant="soft" size="lg">
+            <SportsEsports />
+          </Tooltip>
+        );
+      case "nhl":
+        return (
+          <Tooltip title="NHL" variant="soft" size="lg">
+            <SportsHockey />
+          </Tooltip>
+        );
+      case "minor_league_hockey":
+        return (
+          <Tooltip title="Minor League Hockey" variant="soft" size="lg">
+            <SportsHockey />
+          </Tooltip>
+        );
+      case "family":
+        return (
+          <Tooltip title="Family" variant="soft" size="lg">
+            <Diversity3 />
+          </Tooltip>
+        );
+      case "comedy":
+        return (
+          <Tooltip title="Comedy" variant="soft" size="lg">
+            <Mood />
+          </Tooltip>
+        );
+      case "auto_racing":
+        return (
+          <Tooltip title="Racing" variant="soft" size="lg">
+            <DirectionsCar />
+          </Tooltip>
+        );
+      case "womens_college_volleyball":
+        return (
+          <Tooltip title="Women's College Volleyball" variant="soft" size="lg">
+            <SportsVolleyball />
+          </Tooltip>
+        );
+      default:
+        return <LocalActivity />;
+    }
   };
 
   const handleChangePage = (newPage: number) => {
@@ -68,9 +192,22 @@ export default function ShowsTable() {
     setRange(newValue!);
   };
 
-  const makePerformersString = (performers: Performer[] | undefined) => {
+  const makePerformersString = (
+    performers: Performer[] | undefined,
+    eventType: string | undefined
+  ) => {
     if (performers?.length === 1) {
       return performers[0].name;
+    }
+
+    if (
+      eventType === "nba" ||
+      eventType === "womens_college_volleyball" ||
+      eventType === "nhl" ||
+      eventType === "mls" ||
+      eventType?.includes("ncaa")
+    ) {
+      return `${performers![0].name} vs. ${performers![1].name}`;
     }
 
     let str = "";
@@ -99,6 +236,9 @@ export default function ShowsTable() {
       <Table sx={{ height: "75vh" }}>
         <thead>
           <tr>
+            <th style={{ width: "3%" }}>
+              <Typography level="h4">Type</Typography>
+            </th>
             <th style={{ width: "30%" }}>
               <Typography level="h4">Performers</Typography>
             </th>
@@ -122,7 +262,8 @@ export default function ShowsTable() {
         <tbody>
           {events.map((event) => (
             <tr key={event.id}>
-              <td>{makePerformersString(event.performers)}</td>
+              <td>{setEventTypeIcon(event.type!)}</td>
+              <td>{makePerformersString(event.performers, event.type)}</td>
               <td>
                 <Link
                   href={`https://www.google.com/maps/search/${event.venue?.name?.replaceAll(
@@ -179,7 +320,7 @@ export default function ShowsTable() {
         </tbody>
         <tfoot>
           <tr>
-            <td colSpan={6}>
+            <td colSpan={7}>
               <Box
                 sx={{
                   display: "flex",
@@ -193,7 +334,8 @@ export default function ShowsTable() {
                   <Select onChange={handleChangeRange} value={range}>
                     <Option value={"5mi"}>5 mi</Option>
                     <Option value={"10mi"}>10 mi</Option>
-                    <Option value={"15mi"}>15 mi</Option>
+                    <Option value={"25mi"}>25 mi</Option>
+                    <Option value={"50mi"}>50 mi</Option>
                   </Select>
                 </FormControl>
                 <FormControl orientation="horizontal" size="sm">
